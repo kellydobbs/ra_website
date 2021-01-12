a304db406c3b0a85634a1bf052a0dc4416894c47
Kelly Dobbs <65425386+kellydobbs@users.noreply.github.com>
2021-01-12 13:33:14 -0600

    Add files

---
+++ surveys/index.php
@@ -0,0 +1,101 @@
+<?php include("../_inc/header2.php"); ?>
+
+<div class="row" id="page-header">
+
+
+
+	<img src="../_img/pghead-Products.png">
+
+	
+
+	
+
+	<?php include("../_inc/menu-bar2.php"); ?>
+
+</div>
+
+
+
+
+
+
+
+<div class="row" id="main-page-content">
+
+<div class="inside">
+
+	<h1>Market Research Products</h1>
+
+
+
+	<p>Research America provides market research and call center services to support the outsourcing requirements of famous names in market research and provides telephone surveys and other market research task support to companies just like yours.</p>
+
+
+
+	<ul>
+
+		<li>
+
+			<em>Awareness and Image Studies</em>
+
+		</li>
+
+		<li>
+
+			<em>Market Feasibility and Competitive Intelligence</em>
+
+		</li>
+
+		<li>
+
+			<em>Product Testing and Pricing Research</em>
+
+		</li>
+
+		<li>
+
+			<em>Market Segmentation</em>
+
+		</li>
+
+		<li>
+
+			<em>Purchase Decision Dynamics</em>
+
+		</li>
+
+		<li>
+
+			<em>Customer & Employee Satisfaction and Loyalty Studies</em>
+
+		</li>
+
+		<li>
+
+			<em>Political Polling</em>
+
+		</li>
+
+	</ul>
+
+	
+
+	<h1 class="big-link">Research America provides many helpful and informative services:</h1>
+
+	
+
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+<?php include("../_inc/insight-ux2.php"); ?>
+
+<?php include("../_inc/footer2.php"); ?>
