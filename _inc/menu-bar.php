e7f15a70f72ba602751472f3a3f13b0559a26c7a
Kelly Dobbs <kellydobbs95@gmail.com>
2021-01-13 17:44:47 -0600

    menu-bar file added

---
+++ _inc/menu-bar.php
@@ -0,0 +1,935 @@
+<div class="menu-bar">
+
+	<div class="inner">
+
+		<ul class="main-menu">
+
+			<!--<li>-->
+
+			<!--	<a href="./">-->
+
+			<!--		Home-->
+
+			<!--	</a>-->
+
+			<!--</li>-->
+
+			<?php /* 1 LEVEL */ ?>
+
+			<li>
+
+				<a href="./services">
+
+					Services
+
+				</a>
+
+				
+
+				<?php /* 2 LEVEL */ ?>
+
+				<ul class="cwul-4">
+                    
+                	<li>
+
+					<a href="./services/Innovation-Development.php">Innovation & Development </a>
+
+					
+
+					<?php /* 3 LEVEL */ ?>
+
+					<ul class="cwul-1">
+
+						<li>
+
+							<a href="./services/Ideation-Sessions.php">Concept Ideation & Invention Methods</a>
+
+						</li>
+
+						<li>
+
+		                    <a href="./services/Creative-Problem-Solving-Research.php">Creative Problem Solving Research</a>
+
+						</li>
+
+						<li>
+
+							<a href="./services/Product-Packaging-Evaluation.php">Product & Packaging Evaluation</a>
+
+						</li>
+
+						<li>
+
+							<a href="./services/Consumer-Shopper-Segmentation.php">Consumer / Shopper Segmentation</a>
+
+						</li>
+						<li>
+
+						    <a href="./services/Path-to-Purchase-Journey-Mapping.php">Path to Purchase / Journey Mapping</a>
+
+						</li>
+						<li>
+
+						    <a href="./services/Virtual-Store-Testing.php">Virtual Store Testing</a>
+
+						</li>
+
+					</ul>
+
+			</li>
+        			<li>
+        
+        					<a href="./services/Ad-Tracking-Brand-Messaging.php">Ad Tracking & Brand Messaging </a>
+        
+        					
+        
+        					<?php /* 3 LEVEL */ ?>
+        
+        					<ul class="cwul-1">
+        
+        						<li>
+        
+        							<a href="./services/Brand-Positioning-Research.php">Brand Positioning Research</a>
+        
+        						</li>
+        
+        						<li>
+        
+        		                    <a href="./services/Message-Evaluation-Optimization.php">Message Evaluation Optimization</a>
+        
+        						</li>
+        
+        						<li>
+        
+        							<a href="./services/Ad-Copy-Testing.php">Ad Copy Testing</a>
+        
+        						</li>
+        
+        						<li>
+        
+        							<a href="./services/Reputation-Management.php">Reputation Management</a>
+        
+        						</li>
+        
+        					</ul>
+        
+                    </li>
+                    <li>
+        
+        					<a href="./services/Market-Landscaping-Benchmarking.php">Landscaping & Benchmarking </a>
+        
+        					
+        
+        					<?php /* 3 LEVEL */ ?>
+        
+        					<ul class="cwul-1">
+        
+        						<li>
+        
+        							<a href="./services/Brand-Equity-Assessment.php">Brand Equity Assessment</a>
+        
+        						</li>
+        
+        						<li>
+        
+        		                    <a href="./services/Market-Structure-Landscape.php">Market Structure & Landscape</a>
+        
+        						</li>
+        
+        						<li>
+        
+        							<a href="./services/Competitive-Intelligence.php">Competitive Intelligence</a>
+        
+        						</li>
+        
+        						<li>
+        
+        							<a href="./services/Benchmarking-Market-Research.php">Benchmarking Market Research</a>
+        
+        						</li>
+        						<li>
+        
+        							<a href="./services/Customer-Employee-Satisfaction-Surveys.php">Customer & Employee Satisfaction Surveys</a>
+        
+        						</li>
+        						<li>
+        
+        							<a href="./services/Habits-Practices-Attitudes-Usages.php">Habits & Practices / Attitudes & Usage</a>
+        
+        						</li>
+        
+        					</ul>
+        
+                    </li>
+                    <li>
+        
+        					<a href="./services/Presentation-Result-Reporting.php">Presentation & Reporting</a>
+        
+        					
+        
+        					<?php /* 3 LEVEL */ ?>
+        
+        					<ul class="cwul-1">
+        
+        						<li>
+        
+        							<a href="./services/Data-Presentation-Reporting.php">Data Presentation & Reporting</a>
+        
+        						</li>
+        
+        						<li>
+        
+        		                    <a href="./services/Data-Analysis-Interpretation.php">Data Analysis & Interpretation</a>
+        
+        						</li>
+        
+        						<li>
+        
+        							<a href="./services/Data-Processing-Reduction.php">Data Processing & Reduction</a>
+        
+        						</li>
+        
+        
+        					</ul>
+        
+                    </li>
+                    <li>
+        
+        					<a href="./services/Health-Sustainability.php">Health & Sustainability</a>
+        
+        					
+        
+        					<?php /* 3 LEVEL */ ?>
+        
+        					<ul class="cwul-1">
+        
+        						<li>
+        
+        							<a href="./services/Syndicated.Data.php">Syndicated Data</a>
+        
+        						</li>
+        
+        						<li>
+        
+        		                    <a href="./services/Segmentation-Algorithms-Panels.php">Segmentation Algorithms & Panels</a>
+        
+        						</li>
+        
+        						<li>
+        
+        							<a href="./services/Proprietary-Research-Reports.php">Proprietary Research Reports</a>
+        
+        						</li>
+        
+        
+        					</ul>
+        
+                    </li>
+                    <li>
+
+					<a href="./services/Strategic-Consulting.php">Strategic Consulting </a>
+
+					
+
+					<?php /* 3 LEVEL */ ?>
+
+					<ul class="cwul-1">
+
+						<li>
+
+							<a href="./services/Data-Mining.php">Data Mining</a>
+
+						</li>
+
+						<li>
+
+		                    <a href="./services/Consumer-Insights.php">Consumer Insights</a>
+
+						</li>
+
+						<li>
+
+							<a href="./services/Sensory-Data-Analysis.php">Sensory Data Analysis</a>
+
+						</li>
+
+						<li>
+
+							<a href="./services/Crisis-Communications.php">Crisis Communications</a>
+
+						</li>
+						<li>
+
+						    <a href="./services/Marketing-Business-Strategy.php">Marketing & Business Strategy</a>
+
+						</li>
+
+					</ul>
+
+			</li>
+                    
+					<li>
+
+						<a href="./services/qualitative-market-research.php">Qualitative Marketing Research </a>
+
+						
+
+						<?php /* 3 LEVEL */ ?>
+
+						<ul class="cwul-4-b">
+						    <li>
+
+			<a href="./services/focus-group-facilities.php">Focus Group Facilities</a>
+
+							</li>
+
+							<li>
+
+								<a href="./services/focus-groups.php">Focus Groups</a>
+
+							</li>
+
+							
+
+							<li>
+
+								<a href="./services/face-to-face.php">Face To Face Interviews</a>
+
+							</li>
+
+							<li>
+
+								<a href="./services/interviewing.php">In-Depth Interviews</a>
+
+							</li>
+								<li>
+
+								<a href="./services/Public-Opinion-Community-Engagement.php">Public Opinion</a>
+
+							</li>
+
+						</ul>
+
+				</li>
+                
+				
+
+				<?php /* 2 LEVEL */ ?>
+
+				<li>
+
+					<a href="./services/quantitative-data-collection.php">Quantitative Data Collection</a>
+
+					
+
+					<?php /* 3 LEVEL */ ?>
+                    	<ul class="cwul-1">
+
+						
+							<a href="./services/marketing-research-telephone-interviews.php">Telephone Interviews</a>
+
+						<li>
+
+							<a href="./services/computer-assisted-web-interviewing.php">Web Interviews</a>
+
+						</li>
+
+						<li>
+
+							<a href="./services/online-web-surveys.php">Postal Mail Surveys/Questionnaires</a>
+
+						</li>
+
+						<li>
+
+							<a href="./services/postal-mail-survey.php">Interactive Voice Responses</a>
+
+						</li>
+
+						<li>
+
+							<a href="./services/international.php">Public Opinion & Community Engagement</a>
+
+						</li>
+
+					
+					</ul>
+
+					<!--<ul class="cwul-1">-->
+
+						
+					<!--		<a href="./services/marketing-research-telephone-interviews.php">Marketing Research Interviews</a>-->
+
+					<!--	<li>-->
+
+					<!--		<a href="./services/computer-assisted-web-interviewing.php">Computer Assisted Web Interviewing </a>-->
+
+					<!--	</li>-->
+
+					<!--	<li>-->
+
+					<!--		<a href="./services/online-web-surveys.php">Online Web Surveys</a>-->
+
+					<!--	</li>-->
+
+					<!--	<li>-->
+
+					<!--		<a href="./services/postal-mail-survey.php">Postal Mail Surveys / Self-Administered Questionnaires</a>-->
+
+					<!--	</li>-->
+
+					<!--	<li>-->
+
+					<!--		<a href="./services/international.php">International</a>-->
+
+					<!--	</li>-->
+
+					<!--	<li>-->
+
+					<!--		<a href="./services/interactive-voice-reponses.php">Interactive Voice Reponses</a>-->
+
+					<!--	</li>-->
+
+					<!--</ul>-->
+
+				</li>
+
+				
+
+				<?php /* 2 LEVEL */ ?>
+
+				<!--<li>-->
+
+				<!--	<a href="./services/data-analysis-and-interpretation.php">Data Analysis & Interpretation</a>-->
+
+					
+
+					<?php /* 3 LEVEL */ ?>
+
+				<!--	<ul class="cwul-4-c">-->
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/perceptual-mapping.php">Perceptual Mapping </a>-->
+
+				<!--		</li>-->
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/quadrant-analysis.php">Quadrant Analysis</a>-->
+
+				<!--		</li>-->
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/price-demand-mapping.php">Price-Demand Mapping</a>-->
+
+				<!--		</li>-->
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/correlation-and-regression.php">Correlation & Regression</a>-->
+
+				<!--		</li>-->
+
+						
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/market-segmentation.php">Market Segmentation</a>-->
+
+				<!--		</li>-->
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/conjoint-analysis.php">Conjoint Analysis</a>-->
+
+				<!--		</li>-->
+
+				<!--	</ul>-->
+
+				<!--</li>-->
+
+				
+
+				<?php /* 2 LEVEL */ ?>
+
+				<!--<li>-->
+
+				<!--	<a href="./services/data-processing-and-reduction.php">Data Processing & Reduction</a>-->
+
+					
+
+					<?php /* 3 LEVEL */ ?>
+
+				<!--	<ul class="cwul-4-d">-->
+                    
+    <!--                <li>-->
+
+				<!--			<a href="./services/analytics.php">Analytics </a>-->
+
+				<!--		</li>-->
+
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/coding.php">Coding</a>-->
+
+				<!--		</li>-->
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/programming.php">Programming</a>-->
+
+				<!--		</li>-->
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/cross-tabulations.php">Cross Tabulations</a>-->
+
+				<!--		</li>-->
+
+				<!--	</ul>-->
+
+				<!--</li>-->
+
+				
+
+				<?php /* 2 LEVEL */ ?>
+
+<!--				<li>-->
+
+<!--					<a href="./services/presentation-and-reporting.php">Presentation & Reporting</a>-->
+
+					
+
+					<?php /* 3 LEVEL */ ?>
+
+<!--					<ul class="cwul-4-d">-->
+
+<!--						<li>-->
+
+<!--							<a href="./services/on-site-presentation-and-reporting.php">On-Site</a>-->
+
+<!--						</li>-->
+
+<!--						<li>-->
+
+<!--							<a href="./services/executive-summary.php">Executive Summary</a>-->
+
+<!--						</li>-->
+
+<!--						<li>-->
+
+<!--							<a href="./services/charts-and-graphs.php">Charts &amp; Graphs</a>-->
+
+<!--						</li>-->
+
+<!--						<li>-->
+
+<!--						<a href="./services/key-driver-analysis.php">Key 	Driver Analysis</a>-->
+
+<!--						</li>-->
+
+<!--						<li>-->
+
+<!--							<a href="./services/benchmarking.php">Benchmarking</a>-->
+
+<!--						</li>-->
+<!--<li>-->
+
+							<!-- <a href=".//services/net-promoter-score.php">Net Promoter Score</a> -->
+<!--							<a href="./services/net-promoter-score.php">Net Promoter Score</a>-->
+
+<!--						</li>-->
+<!--					</ul>-->
+
+<!--				</li>-->
+
+				
+
+				<?php /* 2 LEVEL */ ?>
+
+				<!--<li>-->
+
+				<!--	<a href="./services/project-management.php">Project Management</a>-->
+
+					
+
+					<?php /* 3 LEVEL */ ?>
+
+				<!--	<ul class="cwul-2">-->
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/survey-study-design.php">Survey Study Design</a>-->
+
+				<!--		</li>-->
+                        
+    <!--  <li>				<a href="./services/customer-satisfaction-surveys.php">Customer Satisfaction Surveys</a>-->
+
+				<!--		</li>                  -->
+                        
+
+				<!--	  <li>-->
+
+				<!--			<a href="./services/implementation.php">Implementation</a>-->
+
+				<!--		</li>-->
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/quota-control.php">Quota Control</a>-->
+
+				<!--		</li>-->
+
+				<!--		<li>-->
+
+				<!--			<a href="./services/quality-assurance.php">Quality Assurance</a>-->
+
+				<!--		</li>-->
+				<!--		<li>-->
+
+				<!--			<a href="./services/crisis-communications-litigation-research.php">Crisis Management</a>-->
+
+				<!--		</li>-->
+
+				<!--	</ul>-->
+
+				<!--</li>-->
+
+				</ul>
+
+			</li>
+            
+            <li>
+
+				<a href="./about-us/Our-Technology-Products.php">
+
+					Products
+
+				</a>
+
+			</li>
+			<li>
+
+				<?php /* 1ST LEVEL */ ?>
+
+      <!-- <a href=".//about-us-data-collection/research-america-team.php"> About Us </a> -->
+	    <a href="./about-us/research-america-team.php"> About Us </a>
+
+				</a>
+
+				
+
+				<?php /* 2 LEVEL */ ?>
+
+				<ul class="cwul-4">
+                    
+                    <li>
+                        <!--<a href=".//about-us-data-collection/telecommunications-solutions-for-over-40-years.php"> Our Story </a> -->
+                    	<a href="./about-us/leader-in-market-research.php"> Our Story </a> 
+					</li>
+					
+					<li>
+                        <!--<a href=".//about-us/research-america-team.php"> Meet the team </a> -->
+                    	<!--<a href="./about-us/research-america-team.php">Meet the team </a>-->
+                        <a href="./about-us/research-america-team.php">Leadership Team</a>
+					</li>
+					
+					<li>
+                    	<a href="./about-us/Our-Family-of-Companies.php">Company Acquisitions </a> 
+					</li>
+					
+					<li>
+                    	<a href="./clients">Clients</a> 
+                    	<ul class="cwul-3">
+                        
+                         <li>
+                    	<a href="./clients">Clients</a> 
+					   </li>
+                        <li>
+                    	<a href="./resources/case-studies.php">Case Studies </a> 
+					   </li>
+						<li>
+                    	<a href="./resources/RAI-project-summaries-studies.php">Project Summaries </a> 
+					</li>
+						<li>
+
+							<a href="./clients/research-america-testimonials.php">Testimonials</a>
+
+						</li>
+
+					</ul>
+					
+         
+					<!--<li>-->
+						<!-- <a href=".//about-us/diversified-products-and-services.php"> Our Technology Products</a> -->
+					<!--	<a href="../about-us/Our-Technology-Products.php"> Our Technology Products</a>-->
+
+					<!--</li>-->
+
+					<!--<li>-->
+						<!-- <a href=".//about-us/Our-Family-of-Companies.php">  Our Family of Companies</a> -->
+					<!--	<a href="../about-us/Our-Family-of-Companies.php">  Our Family of Companies</a>-->
+					<!--</li>-->
+
+					
+
+				</ul>
+
+			</li>
+
+			<li>
+
+				<?php /* 1ST LEVEL */ ?>
+
+				<a href="./news.php">
+
+					News
+
+				</a>
+
+			</li>
+
+			<li>
+
+				<?php /* 1ST LEVEL */ ?>
+
+				<a href="./industries">
+
+					Industries
+
+				</a>
+
+				<?php /* 2 LEVEL */ ?>
+
+				<ul class="cwul-4">
+
+					<li>
+
+					<a href="./industries/pharmaceutical-market-research.php">Pharmaceutical </a>
+
+					</li>
+
+					<li>
+
+					<a href="./industries/healthcare-market-research.php"> Healthcare  </a>
+
+					</li>
+
+					<li>
+
+						<a href="./industries/retail-industry.php">Retail & Consumer </a>
+
+					</li>
+                    
+                    
+     <li>
+
+						<a href="./industries/agriculture.php">Agriculture </a>
+
+					</li>
+                    
+          <li>
+			<a href="./industries/utilities.php">Utilities </a>
+
+				  </li>  
+                    
+                    
+             <li>
+			   <a href="./industries/manufacturing-industrial-industry.php">Manufacturing </a>
+
+				  </li>  
+					
+					
+			 <li>
+			   <a href="./industries/lottery-market-industry.php">Lottery</a>
+
+				  </li>  		
+					
+					
+                    
+     <li>
+			<a href="./industries/sports-entertainment-tourism-industry.php">Entertainment </a>
+
+				  </li>                  
+                    
+                            
+      <li>
+			<a href="./industries/automotive-industry.php">Automotive</a>
+
+				  </li>                  
+                    
+                    
+		<li>
+			<a href="./industries/financial-legal-industry.php">Finance & Legal</a>
+
+				  </li>                                          
+					
+				 <li>
+			<a href="./industries/food-beverage.php">Food & Beverage</a>
+
+				  </li>
+					
+				 <li><a href="./industries/travel-hospitality.php">Travel & Hospitality</a>
+
+			</li>
+			<li>
+			<a href="./industries/apparel-footwear.php">Apparel & Footwear</a>
+			</li>   
+				  	   	
+			<li>
+			<a href="./industries/home-hardware.php">Home & Hardware</a>
+			</li>   	   	 	
+				  
+				  	   	
+			<li>
+			<a href="./industries/health-beauty-aids.php">Health & Beauty Aids</a>
+			</li>   
+			
+			<li>
+			<a href="./industries/hitech-ecommerce-industry.php">Hi-Tech & Ecommerce </a>
+			</li> 
+				    	  	  	
+		</ul>
+
+			</li>
+
+			<!--<li>-->
+
+				
+
+				<?php /* 1ST LEVEL */ ?>
+
+			<!--	<a href="./clients">-->
+
+			<!--		Clients-->
+
+			<!--	</a>-->
+                
+                
+                <?php /* 2 LEVEL */ ?>
+
+			<!--	<ul class="cwul-5">-->
+
+			<!--		<li>-->
+
+			<!--			<a href="./clients/research-america-testimonials.php"> Testimonials </a>-->
+			<!--		</li>-->
+                        
+                 
+			<!--		<li>-->
+			<!--			<a href="./clients"> Clients </a>-->
+			<!--		</li>-->
+                 
+
+			<!--		</ul>-->
+
+				
+
+			<!--</li>-->
+	        
+	        
+	        <li>
+
+				<?php /* 1ST LEVEL */ ?>
+
+				<a href="./resources/resources.php">
+
+					Resources
+
+				</a>
+                
+                
+                <?php /* 2 LEVEL */ ?>
+
+				<ul class="cwul-5">
+
+					<li>
+					  <a href="./resources/blog.php"> Blog</a>
+					</li>
+					
+				
+					
+					<li>
+					  <a href="./resources/RAI-reports.php">Reports</a>
+					</li>
+                        
+                 
+					<!--<li>-->
+
+					<!--  <a href="../resources/tool-box.php"> Tool Box</a>-->
+					<!--</li>-->
+                        
+					
+					
+					
+					<!--<li>-->
+					<!--	<a href="../resources/case-studies.php"> Case Studies </a>-->
+					<!--</li>-->
+                
+					<li>
+						<a href="./resources/tip-sheets.php"> Tip Sheets </a>
+					</li> 
+					
+						<li>
+					  <a href="./resources/Research-Tools.php">Research Tools</a>
+					</li>
+					<!--<li>-->
+					<!--	<a href="../resources/retail.php"> Retail Report</a>-->
+					<!--</li> -->
+
+					<!--<li>-->
+					<!--	<a href="../resources/RAI-project-summaries-studies.php">Project Summaries</a>-->
+					<!--</li> -->
+
+				
+					<!--<li>-->
+					<!--  <a href="../resources/RAI-healthy-snacking-report.php">Healthy Snacking Report</a>-->
+					<!--</li> -->
+	
+					
+		</ul>
+
+				
+
+			</li>
+
+			<li>
+
+
+				<?php /* 1ST LEVEL */ ?>
+
+				<a href="./contact.php">
+
+					Contact
+
+				</a>
+				
+
+				<?php /* 2ND LEVEL */ ?>
+				<ul class="cwul-5">
+					<li>
+				<a href="./contact.php">  Contact </a>
+					</li>
+					
+					
+					<li>
+					<a href="http://mmsurv.com/panelrecruit">Join Our Panel</a>
+					</li>
+					
+					<li>
+					<a href="./contact/careers.php">Careers</a>
+					</li>
+					
+					
+				</ul>
+
+			</li>
+
+		</ul>
+
+	</div>
+
+</div>
