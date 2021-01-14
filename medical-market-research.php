bdb5ce668b30a720d3446e8dde88ee2e7d75bca5
Kelly Dobbs <kellydobbs95@gmail.com>
2021-01-14 11:07:06 -0600

    blog 1/14

---
+++ medical-market-research.php
@@ -0,0 +1,96 @@
+<?php
+?>      
+  <link rel="stylesheet" href="../_js/owl-carousel/owl.carousel.css">
+  <link rel="stylesheet" href="../_js/owl-carousel/owl.theme.css">
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/jquery.sidr/2.2.1/stylesheets/jquery.sidr.light.min.css">
+  <link rel="stylesheet" href="../_css/style.css">
+  <link rel="stylesheet" href="http://vjs.zencdn.net/5.19.2/video-js.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
+
+    <title>The Latest Medical Market Research Shows Huge Cultural Shift</title>
+    <meta name="description" content="Our latest medical market research indicates that changing demographics and systems of management are improving patient outcomes.">
+    <link rel="canonical" href="https://researchamericainc.com/resources/medical-market-research.php">
+
+<style>
+    a {
+        font-weight:bolder;
+    }
+</style>
+      
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T28PP6C');</script>
+<!-- End Google Tag Manager -->
+
+</head>
+
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T28PP6C"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<div class="container">
+<div class="row" id="main-header">
+  <div class="header-logo">
+    <a href="../">
+      <img src="../_img/research-america-logo-new2.png" class="main-header-logo">
+    </a>
+    <a id="open-mobile-menu" href="#sidr">
+      <img src="../_img/open-mobile-menu.png">
+    </a>
+  </div>
+  <div class="header-number">
+    <p>
+      <a href="tel:1-610-356-1800">Call an Expert!  (610) 356-1800</a>
+    </p>
+  </div>
+</div>
+<!--ooo000ooo -->
+<?php include("../_inc/citiesbar_inc.php"); ?>
+<!--ooo000ooo -->
+<!-- above div with id page-header -->
+<div class="row" id="page-header">
+
+	<img src="../_img/medical-market-research.png" alt="medical market research trends"> <!-- header image: 1000 × 290 -->
+
+	<?php include("../_inc/menu-bar2.php"); ?>
+
+</div>
+
+
+
+
+
+<div class="row" id="main-page-content">
+
+<div class="inside">
+
+<h1>The Latest Medical Market Research Shows Huge Cultural Shift</h1>
+<br>
+<p>Markets fluctuate. They ebb and flow according to supply and demand issues, public policy, technological and environmental concerns and even patterns of disease (“hello, COVID-19!”). Some markets experience rapid change; but others, such as the U.S. healthcare industry, are slower to transform, with both social and governmental constraints combining to create a system that is resistant to expeditious shifts in focus or outcomes. But this doesn’t mean <a href="https://researchamericainc.com/resources/why-hire-a-market-research-firm.php">quality market research</a> is any less important within the medical field. On the contrary, the healthcare industry’s seemingly apparent stagnation just makes it harder to anticipate when and where and how the tide actually <em>will</em> turn, thus making it even more paramount for medical companies to keep an ever-present and watchful eye on all healthcare-related influences and effects. For instance, our latest work here at <a href="https://researchamericainc.com/industries/healthcare-market-research.php">Research America</a> indicates a cultural shift is currently at play within the healthcare industry. We’ve long anticipated value-based care becoming a leading driver within the medical field; innovations in technology ensure that access to quality services will eventually make medical care more affordable and more effective. But our research indicates that changing demographics and systems of management are improving patient outcomes, as well. Here’s what we’ve learned:</p>
+<br>
+<h2 class="med-link">Care is Becoming Community-Based</h2>
+<p>The Affordable Healthcare Act has expanded healthcare to a more <a href="https://www.forbes.com/sites/glennllopis/2015/07/13/the-cultural-demographic-shift-is-changing-the-business-of-healthcare/?sh=648090bf33c5" target="_blank">diverse group of people,</a> and these people are wanting services — as well as caregivers — that represent and cater to their unique experiences. As a result, medical market researchers need to find ways to identify and connect with different demographics in order to garner insights that fully inform good medical business decisions.</p>
+<br>
+<h2>Patient Reported Outcomes are More Important Than Ever</h2>
+<p>Quality care is no longer based solely on the opinions of the physicians and clinicians who provide it. Nor is it only the product of traditional care services. Instead, good care is becoming increasingly patient-centric. Not only do patients have more to say about their level of care, they have more control over how they receive it. (COVID-19 has made sure of that, allowing telemedicine solutions to move from the periphery to the forefront of quality care resources.) This means that elements of the patient experience will need to be monitored and reviewed so that companies can improve upon their products and services based on patient views (not just professional ones).</p>
+<br>
+<h2 class="med-link">People Management is Key</h2>
+<p><a href="https://www.statnews.com/2020/05/19/9-ways-covid-19-forever-upend-health-care/" target="_blank"><span>COVID-19</span></a> has also delivered another likely permanent change to the healthcare market: a focus on personnel. As the pandemic has spread, renewed appreciation for essential workers of all kinds has blossomed. Healthcare is <a href="https://www.ama-assn.org/practice-management/medicare/cms-offers-more-flexibility-financial-stability-covid-19-care" target="_blank">no longer restricted by specific degrees and buildings</a>, but dependent on a host of players outside traditional scenes of care. The U.S. healthcare system has been stretched thin for a long time; now that COVID-19 has shown what is possible when alternative workers provide care, it’s probable that the medical industry as a whole will begin evaluating how to shift personnel responsibilities to best suit future healthcare demands. The result: medical market research needs to evaluate medical personnel to find opportunities that both maximize their expertise and validate their experiences so that patient outcomes continue to be deemed beneficial.</p>
+<br>
+<h2 class="med-link">Want to Learn More?</h2>
+<p>Of course, custom medical market research is an individualized endeavor and should take into account a variety of factors unique to those conducting it. If you are in the medical industry and would like to learn more about key influencers within the healthcare market so that you can make decisions that better the lives of patients, as well as providers, please <a href="https://researchamericainc.com/about-us/research-america-team.php">contact our team</a> at Research America.</p>
+<br>
+</div>
+</div>
+
+
+<?php include("../_inc/insight-ux2.php"); ?>
+
+<?php include("../_inc/footer2.php"); ?>
