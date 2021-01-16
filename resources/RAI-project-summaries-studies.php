3266fc6fcd292ca52f7ac168b04663333052ba95
Kelly Dobbs <kellydobbs95@gmail.com>
2021-01-15 23:28:15 -0600

    update GTM

--- resources/RAI-project-summaries-studies.php
+++ resources/RAI-project-summaries-studies.php
@@ -29,12 +29,7 @@
   <link href="../_css/style.css" rel="stylesheet">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TJ6M582');</script>
-<!-- End Google Tag Manager -->
+<?php include("../_inc/gtm.php"); ?>
 </head>
 
 <body>
