4e4cf403762f1782abf4022df6f256d8b5e2c260
Kelly Dobbs <kellydobbs95@gmail.com>
2021-01-19 15:08:39 -0600

    1/19 blog

---
+++ resources/market-research-vs-marketing-research.php
@@ -0,0 +1,88 @@
+<?php
+?>      
+  <link rel="stylesheet" href="../_js/owl-carousel/owl.carousel.css">
+  <link rel="stylesheet" href="../_js/owl-carousel/owl.theme.css">
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/jquery.sidr/2.2.1/stylesheets/jquery.sidr.light.min.css">
+  <link rel="stylesheet" href="../_css/style.css">
+  <link rel="stylesheet" href="http://vjs.zencdn.net/5.19.2/video-js.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
+
+    <title>Market Research vs. Marketing Research: What’s the Difference?</title>
+    <meta name="description" content="Market research vs marketing research: Although they sound quite similar, there is an important difference. Learn how they differ in this blog.">
+    <link rel="canonical" href="https://researchamericainc.com/resources/market-research-vs-marketing-research.php">
+
+<style>
+    a {
+        font-weight:bolder;
+    }
+</style>
+<?php include("../_inc/gtm.php"); ?> 
+</head>
+<body>
+<?php include("../_inc/gtm-body.php"); ?>
+
+
+<div class="container">
+<div class="row" id="main-header">
+  <div class="header-logo">
+    <a href="../">
+      <img src="../_img/research-america-logo-new2.png" class="main-header-logo">
+    </a>
+    <a id="open-mobile-menu" href="#sidr">
+      <img src="../_img/open-mobile-menu.png">
+    </a>
+  </div>
+  <div class="header-number">
+    <p>
+      <a href="tel:1-610-356-1800">Call an Expert!  (610) 356-1800</a>
+    </p>
+  </div>
+</div>
+<!--ooo000ooo -->
+<?php include("../_inc/citiesbar_inc.php"); ?>
+<!--ooo000ooo -->
+<!-- above div with id page-header -->
+<div class="row" id="page-header">
+
+	<img src="../_img/market-research-vs-marketing-research.png" alt="the difference between market research and marketing research"> <!-- header image: 1000 × 290 -->
+
+	<?php include("../_inc/menu-bar2.php"); ?>
+
+</div>
+
+
+
+
+
+<div class="row" id="main-page-content">
+
+<div class="inside">
+
+<h1>Market Research vs. Marketing Research: What’s the Difference?</h1>
+<p><span >In the world of research, nuance is king. The varying outcomes of any study — regardless of its focus or its methodology — rely on an attention to detail held in such high esteem that subtle effects are neither mismanaged nor misunderstood, but rather searched for and celebrated. Indeed, the veracity of both the research process, as well as the documentation of its findings, depends on people being able to determine just what’s important and what isn’t. This is hardly surprising. Research is about</span><a href="http://www.jsu.edu/depart/psychology/sebac/fac-sch/rm/Ch4-4.html" rel="noopener" target="_blank"><span> discovery</span></a><span >, after all, the uncovering of new information from facts and perceptions and opinions that might seem insignificant at first but that actually inform critical insight if only viewed through the right lens. This is why it’s so important that the foundation of any research project be fully understood prior to data collection. Take, for instance, market research vs. marketing research. While it might seem inconsequential, the difference between the two terms — that “-ing” — is enormous, necessitating vastly different research strategies and yielding vastly different results. Here’s a quick review of market research vs. marketing research and why the differences between the two matter:</span></p>
+<p><span >&nbsp;</span></p>
+<h2 class="med-link"><span >Marketing Research is Broad; Market Research is Specific</span></h2>
+<p><span >Marketing research is a broad term meant to represent all the activities a company undertakes in order to uncover information relating to the</span><a href="https://www.investopedia.com/terms/f/four-ps.asp" rel="noopener" target="_blank"><span> four Ps of marketing</span></a><span >: product, price, place and promotion. Market research, on the other hand, is specific, focusing on only one part of the marketing mix, “place.” In this way, market research is a subset of marketing research. The</span><a href="https://wikidiff.com/depth/breadth" rel="noopener" target="_blank"><span> subtle difference</span></a><span > between the two terms shifts research priorities and, as a result, the planning and execution of each one’s research objective(s). Marketing research emphasizes a greater breadth of data collection while market research concentrates on gathering facts and figures on a smaller, but more in-depth, scale. To expect one to deliver the other’s scope of results needlessly wastes resources and only confounds overall research discovery.</span></p>
+<p>&nbsp;</p>
+<h2 class="med-link"><span >Marketing Research Studies The Marketing Process; Market Research Studies Market &amp; Consumer Behavior</span></h2>
+<br>
+<span >Note here that the ultimate goal of a market research campaign is to determine the viability of a certain product or service in a given target market. This information can prove invaluable for a business looking to launch a new product. As such, </span><span >market research is often the</span><a href="https://researchamericainc.com/resources/market-research-for-startups.php" rel="noopener"><span> first step</span></a><span > companies take as they begin their research activities.</span><span > But information gleaned from market research may alter key business perceptions about their own products, customers, and research techniques. What's more, market research allows businesses to focus on specific details relating to consumer behaviors in particular markets.&nbsp;</span></p>
+<p>&nbsp;</p>
+<p><span >As one might imagine, broad marketing research complements the granular nature of market research. While market research can help businesses assess certain target markets and product performance in those markets, marketing research offers companies greater tranches of information relating to numerous marketing variables. Together, they enable businesses to make informed, detailed decisions that can significantly improve large-scale marketing strategies.&nbsp;&nbsp;</span></p>
+<p><br><br></p>
+<p><span >&nbsp;</span></p>
+<h2 class="med-link"><span >Learn More with the Right Data Collection Partner</span></h2>
+<p><span >Of course, these are just a few of the differences between market research and marketing research. Too often people — researchers included — jump to conclusions, dismissing precision just so they can get something done. In the business world, this has serious implications. Putting the proverbial cart before the horse can lead to unfortunate assumptions that confuse issues, negate opportunity, inhibit growth and, ultimately, ruin a company’s chances for success. Our</span><a href="https://researchamericainc.com/about-us/Our-Family-of-Companies.php"><span> team</span></a><span > at Research America understands the different kinds of</span><a href="https://smallbusiness.chron.com/business-research-43341.html" rel="noopener" target="_blank"><span> business research</span></a><span > and can</span><a href="https://researchamericainc.com/resources/why-hire-a-market-research-firm.php"><span> help your company</span></a><span > choose the right tools to extract meaningful and actionable insight from consumers, customers and/or markets, whichever audience offers the most fitting potential for solving the problem at hand. Please</span><a href="https://researchamericainc.com/"><span> contact us</span></a><span > to learn more.</span></p>
+<p><span >&nbsp;</span></p>
+<p>&nbsp;</p>
+
+
+</div>
+</div>
+
+
+<?php include("../_inc/insight-ux2.php"); ?>
+
+<?php include("../_inc/footer2.php"); ?>
