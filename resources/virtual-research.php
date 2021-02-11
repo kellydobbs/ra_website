542455cd60e4e4cece372c2530b9e3b54a12a3cc
Kelly Dobbs <kellydobbs95@gmail.com>
2021-02-10 20:09:42 -0600

    blog 2/18

---
+++ resources/virtual-research.php
@@ -0,0 +1,86 @@
+<?php
+//  $pageTitle = " How to Use Focus Groups for Consumer Insights";
+//  include("../_inc/header2.php");
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+  <link rel="stylesheet" href="../_js/owl-carousel/owl.carousel.css">
+  <link rel="stylesheet" href="../_js/owl-carousel/owl.theme.css">
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/jquery.sidr/2.2.1/stylesheets/jquery.sidr.light.min.css">
+  <link href="../_css/style.css" rel="stylesheet">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">     
+
+
+    <title>Virtual Research: A Good Idea or a Bad Experiment?</title>
+    <meta name="description" content="Technology is creating new avenues for gaining consumer insights through virtual research. Here are 3 reasons why virtual market research is a gamechanger.">
+
+<?php include("../_inc/gtm.php"); ?>
+</head>
+<body>
+<?php include("../_inc/gtm-body.php"); ?>
+
+
+<div class="container">
+<div class="row" id="main-header">
+  <div class="header-logo">
+    <a href="../">
+      <img src="../_img/research-america-logo-new2.png" class="main-header-logo">
+    </a>
+    <a href="/" class="home_top">HOME</a>
+    <a id="open-mobile-menu" href="#sidr">
+      <img src="../_img/open-mobile-menu.png">
+    </a>
+  </div>
+  <div class="header-number">
+    <p>
+      <a href="tel:1-610-356-1800">Call an Expert!  (610) 356-1800</a>
+    </p>
+  </div>
+</div>
+<!--ooo000ooo -->
+<?php include("../_inc/citiesbar_inc.php"); ?>
+<!--ooo000ooo -->
+<!-- above div with id page-header -->
+<div class="row" id="page-header">
+
+	<img src="../_img/virtual-research.jpg" alt="conducting virtual research"> <!-- header image: 1000 × 290 -->
+
+	<?php include("../_inc/menu-bar2.php"); ?>
+  
+</div>
+
+<div class="row" id="main-page-content">
+<div class="inside">
+
+
+<h1>Virtual Research: A Good Idea or a Bad Experiment?</h1>
+<p>Market research is vital for creating successful business plans that are profitable to organizations. To grow and thrive, companies have to carefully survey their surroundings and attend to all their various parts, ensuring that they are both nourished and strengthened for whatever lies ahead of them, as well as properly recovered from any damages in their past. It’s not unlike the care we strive to give our own bodies, simultaneously protecting and healing ourselves via regular check-ins with an expert. Lucky for us, just as technology continues to broaden the confines of modern medicine, it continues to expand the world of <a href="https://www.investopedia.com/terms/m/market-research.asp" target="_blank"><span>market research</span></a>, allowing companies the ability to better monitor and assess the health of their business opportunities through ongoing market analysis. Thus, despite many people lamenting loss of intimacy due to an ever-increasing “digital disconnect” (i.e., that perceived sense of alienation from one another that sometimes occurs as virtual interactions replace physical ones), the benefit of remote communication is no less valuable nor any less effective, especially within a research setting. Some might argue that there isn’t even a loss of intimacy at all, but rather a gain, as people around the globe are united instead of kept separated by time and space.</p>
+<p>&nbsp;</p>
+<p>For us here at <a href="https://researchamericainc.com/" ><span>Research America</span></a>, the advantages of technology are clear: with it we are able to reach more people, gather more exhaustive insight and help more businesses. Here’s why virtual research, in particular, is not just a good idea, it’s a great one:</p>
+<p>&nbsp;</p>
+<h2 class="med-link">It’s Fast</h2>
+<p>Conducting market research virtually means more time can be spent on strategizing and planning and less time on the actual execution of it. Many virtual research platforms yield data in real-time (or close to it!), collecting and transcribing information at the same time so that analysis often begins the moment a respondent relays his or her first bit of insight. Many traditional forms of market research just can’t approximate that type of speed.</p>
+<p>&nbsp;</p>
+<h2 class="med-link">It’s Cost-Effective</h2>
+<p>Virtual research is also frequently less expensive than other forms of market research. There’s no need for paper, printing or postage. And because respondents don’t have to be gathered in one place (as with <a href="https://smallbiztrends.com/2020/07/what-is-a-focus-group.html" target="_blank"><span>focus groups</span></a>, for example), companies don’t have to worry about any costs for travel, food, accommodation or pay for facilitators.&nbsp;</p>
+<p>&nbsp;</p>
+<h2 class="med-link">It’s Often More Accurate Because It’s Convenient and Flexible</h2>
+<p>Furthermore, virtual research methods facilitate an increased flexibility in design that means results are often more accurate than those gathered by many traditional ones. Indeed, most virtual research tools can be easily modified according to the people using them, with certain questions skipped or changed depending on individual characteristics and interests and access to the survey tool allowed whenever and however a respondent chooses. This means respondents are better engaged and, thus, more likely to complete a research request, improving the response rate and making results more indicative of a population.</p>
+<p>&nbsp;</p>
+<h2 class="med-link">It’s Safe</h2>
+<p>Finally, virtual research is safe. Unlike in-person research encounters, virtual research enables businesses to continue surveying the market without exposing their members to unnecessary risks. And during a pandemic, that might be the best reason to make use of it!</p>
+<p>&nbsp;</p>
+<h2 class="med-link">Want to Learn More?</h2>
+<p>Of course, virtual research isn’t always the answer. <a href="https://researchamericainc.com/resources/choosing-between-online-and-in-person-qualitative.php" ><span>Sometimes a face-to-face encounter is necessary</span></a> in order to uncover nuance and delve into the specifics of an issue. Fortunately, our team at Research America has decades of experience crafting <em>all</em> kinds of market research projects, not just virtual ones. In fact, oftentimes the most meaningful research results stem from a mixed-mode approach, combining both remote and in-person methodologies so that resulting insight is more comprehensive, more representative and, thus, more effective when put to action. Please <a href="https://researchamericainc.com/contact.php" ><span>contact us</span></a> to learn which research tools would best suit your own unique research problem.</p>
+
+</div>
+</div>
+
+
+<?php include("../_inc/insight-ux2.php"); ?>
+
+<?php include("../_inc/footer2.php"); ?>
