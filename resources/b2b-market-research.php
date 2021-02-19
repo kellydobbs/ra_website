7e0b55ec43371bc0cc71786a53ba8c62c82ee9a9
leapclixx <72580559+leapclixx@users.noreply.github.com>
2021-02-19 11:34:26 -0600

    Add files via upload
    New blog 2/19

---
+++ resources/b2b-market-research.php
@@ -0,0 +1,89 @@
+<?php
+//  $pageTitle = "A 5-Minute Guide to Understanding B2B Market Research";
+//  include("../_inc/header2.php");
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+  <link rel="stylesheet" href="../_js/owl-carousel/owl.carousel.css">
+  <link rel="stylesheet" href="../_js/owl-carousel/owl.theme.css">
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/jquery.sidr/2.2.1/stylesheets/jquery.sidr.light.min.css">
+  <link href="../_css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">     
+
+    <title>A 5-Minute Guide to Understanding B2B Market Research</title>
+    <meta name="description" content="Want to understand B2B Market Research? Take five minutes and read our guide to get up to speed.">
+
+<?php include("../_inc/gtm.php"); ?> 
+</head>
+<body>
+<?php include("../_inc/gtm-body.php"); ?>
+
+
+<div class="container">
+<div class="row" id="main-header">
+<div class="header-logo">
+    <a href="../">
+      <img src="../_img/research-america-logo-new2.png" class="main-header-logo">
+    </a>
+    <a href="/" class="home_top">HOME</a>
+    <a id="open-mobile-menu" href="#sidr">
+      <img src="../_img/open-mobile-menu.png">
+    </a>
+  </div>
+  <div class="header-number">
+    <p>
+      <a href="tel:1-610-356-1800">Call an Expert!  (610) 356-1800</a>
+    </p>
+  </div>
+</div>
+<!--ooo000ooo -->
+<?php include("../_inc/citiesbar_inc.php"); ?>
+<!--ooo000ooo -->
+<!-- above div with id page-header -->
+<div class="row" id="page-header">
+
+	<img src="../_img/b2b-market-research.jpg" alt="b2b market research"> <!-- header image: 1000 × 290 -->
+
+	<?php include("../_inc/menu-bar2.php"); ?>
+
+</div>
+
+
+
+
+
+<div class="row" id="main-page-content">
+
+<div class="inside">
+
+<h1>A 5-Minute Guide to Understanding B2B Market Research</h1>
+<p>&nbsp;</p>
+<p>Business to business or <a href="https://www.aqr.org.uk/glossary/b2b-research" target="_blank"><span>B2B market research</span></a> is the process of gathering information about the consumers, products and services within the business-to-business market. As with all market research, B2B market research is used to collect information so that better business decisions can be made. The actual execution of the research endeavor might resemble B2C (business to consumer) market research interactions, with surveys, focus groups, interviews and <a href="https://researchamericainc.com/resources/Ethnographic-Research.php" ><span>other commonly utilized methodologies</span></a> used to gather insight into the research problem. However, the strategies behind B2B market research rest on significantly different concerns, making it paramount that B2B researchers recognize fundamental trade characteristics, as well as specific industry- and company- related facts and figures. One size market research definitely does NOT fit all in a business to business context! Here’s a quick summary of B2B market research:</p>
+<p>&nbsp;</p>
+<h2 class="med-link">Why B2B Market Research is Different From B2C Market Research</h2>
+<p>The goal of both B2B and B2C transactions is to sell products and/or services. For B2C consumers, personal satisfaction is often the deciding factor for purchasing behavior. For B2B consumers, however, satisfaction is spread across a variety of stakeholders, making it vital that researchers understand the B2B sales cycle and <em>all</em> the buyer personas involved within it. It can be a difficult task, as the B2B sales cycle changes according to the type of industry and even the individual companies involved.</p>
+<p>&nbsp;</p>
+<h2 class="med-link">3 Big Reasons for the Differences Between B2B and B2C Market Research&nbsp;</h2>
+<p>As mentioned, the single biggest reason B2B market research is different from B2C market research resides in the B2B consumer. Put simply, there are fewer consumers in the B2B market, and they participate in a much more <a href="https://smallbusiness.chron.com/8-steps-business-organizations-purchasing-process-2267.html" target="_blank"><span>complex purchasing process</span></a>. Not only must B2B market researchers have a firm grasp of the people making purchasing decisions within an industry and/or company, they have to know how to identify and then actually make contact with them! With fewer overall consumers from which to choose, the process for picking the right one(s) can be particularly arduous. People change positions. They move to new jobs at new companies and begin new partnerships with new priorities. Thus, every B2B relationship is a unique one. Researchers can’t automatically assume that the same person (or type of person) will have the right information at any given time. Instead, they must <a href="https://researchamericainc.com/services/Creative-Problem-Solving-Research.php"><span>problem solve</span></a> within the context of a certain business field, finding connections that actually garner appropriate data for the problem at hand each and every time a project begins.</p>
+<p>&nbsp;</p>
+<p>Furthermore, the stakes in the B2B market are frequently higher than in a B2C one since B2B products are usually more specialized, more complex and more likely to be distributed via multiple <a href="https://www.investopedia.com/terms/d/distribution-channel.asp" target="_blank"><span>channels</span></a> in larger quantities than those in a B2C setting. The result: higher risks that researchers absolutely have to appreciate if any meaningful assessment of them is to take place.</p>
+<p>&nbsp;</p>
+<p>And finally, the B2B sales cycle is long with many more stages and many more players than the B2C buying journey. And with profit margins and bottom lines to consider, B2B decisions are often less emotional and more rational than those an individual consumer might make, as well. Market researchers have to help their clients identify and prioritize the most meaningful features influencing the market so that only the most <em>relevant</em> information is considered and the most applicable insight received.</p>
+<p>&nbsp;</p>
+<h2 class="med-link">Want to Learn More?</h2>
+<p>B2B market research is a unique field, necessitating <a href="https://researchamericainc.com/resources/why-hire-a-market-research-firm.php" ><span>experts</span></a> that know how to <a href="https://researchamericainc.com/resources/ten-tips-for-recruiting-hard-to-find-participants.php" ><span>identify and reach the right research respondents</span></a>, as well as design assessments that highlight only the most reliable of results. Our team at Research America can help your company plan, create, distribute and evaluate B2B market research that improves your products, services and business relationships and also increases your profitability. Please <a href="https://researchamericainc.com/contact.php" ><span>contact us</span></a> to learn more.</p>
+<p><br><br></p>
+
+
+
+</div>
+</div>
+
+
+<?php include("../_inc/insight-ux2.php"); ?>
+
+<?php include("../_inc/footer2.php"); ?>
