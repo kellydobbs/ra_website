b62bd964d4e8986c319125c84dc7e42f85566dcf
Kelly Dobbs <kellydobbs95@gmail.com>
2021-02-19 10:54:14 -0600

    hispanic market research

---
+++ resources/hispanic-market-research.php
@@ -0,0 +1,94 @@
+<?php
+//  $pageTitle = " How to Use Focus Groups for Consumer Insights";
+//  include("../_inc/header2.php");
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+  <link rel="stylesheet" href="../_js/owl-carousel/owl.carousel.css">
+  <link rel="stylesheet" href="../_js/owl-carousel/owl.theme.css">
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/jquery.sidr/2.2.1/stylesheets/jquery.sidr.light.min.css">
+  <link href="../_css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">     
+
+    <title>Hispanic Market Research: How It’s Changed in the Past 15 Years</title>
+    <meta name="description" content="We are seeing new opportunities in hispanic market research because of ever-changing and growing demographics.">
+
+<?php include("../_inc/gtm.php"); ?> 
+</head>
+<body>
+<?php include("../_inc/gtm-body.php"); ?>
+
+
+<div class="container">
+<div class="row" id="main-header">
+  <div class="header-logo">
+    <a href="../">
+      <img src="../_img/research-america-logo-new2.png" class="main-header-logo">
+    </a>
+    <a href="/" class="home_top">HOME</a>
+    <a id="open-mobile-menu" href="#sidr">
+      <img src="../_img/open-mobile-menu.png">
+    </a>
+  </div>
+  <div class="header-number">
+    <p>
+      <a href="tel:1-610-356-1800">Call an Expert!  (610) 356-1800</a>
+    </p>
+  </div>
+</div>
+<!--ooo000ooo -->
+<?php include("../_inc/citiesbar_inc.php"); ?>
+<!--ooo000ooo -->
+<!-- above div with id page-header -->
+<div class="row" id="page-header">
+
+	<img src="../_img/hispanic-market-research.jpg" alt="hispanic market research"> <!-- header image: 1000 × 290 -->
+
+	<?php include("../_inc/menu-bar2.php"); ?>
+
+</div>
+
+
+
+
+
+<div class="row" id="main-page-content">
+
+<div class="inside">
+
+<h1>Hispanic Market Research: How It’s Changed in the Past 15 Years</h1>
+
+<p>America is changing. If 2020 wasn’t a clear indication of the forces reshaping our nation, then consider some of the following statistics compiled by <a href="https://www.pewresearch.org/fact-tank/2017/09/18/how-the-u-s-hispanic-population-is-changing/" target="_blank" ><span>Pew Research Center</span></a>, PBS and more:</p>
+<ul>
+<li>In just the past 15 years, the U.S. Hispanic population has increased from 42 million to just over 60 million, and it is projected that by 2050, that number will reach 128 million!</li>
+<li>Approximately <a href="https://www.pbs.org/newshour/nation/3-ways-that-the-u-s-population-will-change-over-the-next-decade" target="_blank"><span>19%</span></a> of the total U.S. population consider themselves Hispanic. While the Hispanic population’s rate of growth has been slowing since 2005, experts project that Latinos will account for 29% of the U.S. population by 2050 (up from 14% in 2005).</li>
+<li>Hispanics are among the <a href="https://www.pewresearch.org/fact-tank/2018/09/13/key-facts-about-young-latinos/" target="_blank"><span>youngest ethnic groups</span></a> in the U.S. with a median age of 30 years in 2019.</li>
+<li>Today, the majority of Hispanics living in the U.S. are born in America: <a href="https://www.pewresearch.org/fact-tank/2018/09/13/key-facts-about-young-latinos/" target="_blank"><span>81%</span></a> of those 35 years and younger were born here in the U.S. rather than abroad.</li>
+</ul>
+<p>Obviously, the U.S is undergoing a fundamental shift in its demographic makeup. That’s what makes it so critical for companies to utilize solid market research strategies. During tumultuous times like these, understanding the forces shaping current market conditions can help businesses craft meaningful products and procedures that address <em>real</em> — not perceived — issues. Here’s a brief overview of why Hispanic market research is critical in 2021:</p>
+<p>&nbsp;</p>
+<h2 class="med-link">Consumer Profiles are Different Now</h2>
+<p>Because the Hispanic population in the U.S. is so large now, and because its median age is 30 years, the overall American consumer landscape is significantly different than even just a few years ago. Both the age of and quantity (of individuals) within a market carry great significance since technological habits and interests and purchasing power are all necessarily affected by the characteristics of the people yielding them. Hispanic market research is needed in order to fully grasp all relevant consumer personas in 2021 and beyond so that companies can meet consumer demand, reach more people, increase <a href="https://researchamericainc.com/resources/brand-loyalty-research.php" ><span>brand loyalty</span></a> and make more sales!</p>
+<br>
+<h2 class="med-link">Language Is Important</h2>
+<p>Depending on age and location, many Hispanics will respond to different language-based marketing efforts, with older, foreign-born Hispanics often preferring Spanish content and younger, native Hispanic Americans often looking for a more English-based or bilingual approach. Hispanic market research helps determine when and where and how to target specific audiences through language and cultural messaging.</p>
+<p>&nbsp;</p>
+<h2 class="med-link">Income and Level of Education Have Increased</h2>
+<p>More Hispanics are going to college; more have <a href="https://www.wsj.com/articles/hispanic-household-income-climbs-1536792308" target="_blank"><span>jobs</span></a> and are buying houses. Hispanics have more purchasing power than at any other time in our history. This growth has exciting implications for companies if they use Hispanic market research to identify specific wants and needs.</p>
+<p>&nbsp;</p>
+<h2 class="med-link">Ready to Learn More?</h2>
+<p>Hispanics account for nearly 19% of the U.S.’s total population, making the Hispanic market a sizable and profitable opportunity for those companies willing to probe it with actionable <a href="https://researchamericainc.com/services/" ><span>market research</span></a>. To learn more, please <a href="https://researchamericainc.com/contact.php" ><span>contact our team at Research America</span></a>. Our expert researchers can help you utilize Hispanic market research for meaningful — actionable — insight.</p>
+<p>&nbsp;</p>
+
+
+</div>
+</div>
+
+
+<?php include("../_inc/insight-ux2.php"); ?>
+
+<?php include("../_inc/footer2.php"); ?>
